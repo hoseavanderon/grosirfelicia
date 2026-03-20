e73ddad72262af7f27c5ae985f87ebbda1844a1f
hoseavanderson <hosea.vanderson2003@gmail.com>
2026-03-20 22:34:43 +0800

    asdasd

--- resources/views/barang-masuk.blade.php
+++ resources/views/barang-masuk.blade.php
@@ -17,8 +17,9 @@
     </style>
     <section class="section">
 
-        <div class="d-flex justify-content-between align-items-center mb-3">
-            <h5 class="mb-0 fw-bold">Tambah Barang Masuk</h5>
+        <div class="section-header d-flex justify-content-between align-items-center">
+            <h1>Barang Masuk</h1>
+
             <div class="d-flex gap-2">
                 <button type="button" id="add-row" class="btn btn-primary">
                     + Tambah Baris
